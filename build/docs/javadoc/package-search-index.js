d757d1ca33942f084a854b86c23ffdee54160e6e
Uday Bhaskar <udabhas@amazon.com>
2025-11-20 11:39:04 +0530

    Remove build artifacts from git tracking

--- build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.opensearch.crypto"},{"l":"org.opensearch.crypto.generic"}];updateSearchResults();
--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.opensearch.crypto.generic","l":"GenericCryptoHandler"},{"p":"org.opensearch.crypto.generic","l":"GenericCryptoPlugin"},{"p":"org.opensearch.crypto","l":"GenericCryptoPlugin"},{"p":"org.opensearch.crypto","l":"SimpleCryptoHandler"}];updateSearchResults();
